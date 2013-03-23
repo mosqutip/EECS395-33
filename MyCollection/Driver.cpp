22cf7e1339af107da5242ee6ea1cf62c1b8c590a
John Greene <johngreene2012@u.northwestern.edu>
2013-03-22 23:10:57 -0500

    parent class working

--- MyCollection/Driver.cpp
+++ MyCollection/Driver.cpp
@@ -6,13 +6,25 @@ using namespace std;
 int main ()
 {
     int myints[] = { 75, 23, 65, 42, 13 };
-    MyVector<int, 5> vect(myints, myints+5);
-    MyVector<int, 5>::iterator it(vect, 4);
-    for (it; it != --vect.begin(); --it)
+    MyVector<int> vect(myints, myints+5);
+    MyVector<int>::iterator it = vect.end();
+	MyCollection<int>* p1 = &vect;
+	MyCollection<int>::iterator i2 = p1->end();
+	i2--;
+	/*
+	for (it; it != vect.begin(); --it)
     {
+		printf("here");
         printf("%d\n", *it);
     }
-    MyVector<int, 5> myv;
+	*/
+	for (i2; i2 != p1->begin(); --i2)
+    {
+		printf("here");
+        printf("%d\n", *i2);
+    }
+	
+    MyVector<int> myv;
     int i = myv.starting_element();
     int j = myv.last_element();
     printf("%d %d\n", i, j);
--- MyCollection/MyCollection.h
+++ MyCollection/MyCollection.h
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-template <class T, int N>
+template <class T>
 class MyIterator;
 
-template <class T, int N>
+template <class T>
 class MyCollection;
 
-template <class T, int N>
-class MyVector : public MyCollection<T, N>
+template <class T>
+class MyVector : public MyCollection<T>
 {
-    friend class MyIterator<T, N>;
+    friend class MyIterator<T>;
 
     private:
 
@@ -20,12 +20,14 @@ class MyVector : public MyCollection<T, N>
          vector<int> vect;
 
     public:
+		/*
         typedef MyIterator<T, N> iterator;
         typedef ptrdiff_t difference_type;
         typedef size_t size_type;
         typedef T value_type;
         typedef T *pointer;
         typedef T &reference;
+		*/
 
         int &operator[] (int i) {
             return vect[i];
@@ -48,10 +50,10 @@ class MyVector : public MyCollection<T, N>
         }
 };
 
-template <class T, int N>
+template <class T>
 class MyCollection
 {
-    friend class MyIterator<T, N>;
+    friend class MyIterator<T>;
 
     private:
 
@@ -59,21 +61,30 @@ class MyCollection
         int size;
         
     public:
+        typedef MyIterator<T> iterator;
+        typedef ptrdiff_t difference_type;
+        typedef size_t size_type;
+        typedef T value_type;
+        typedef T *pointer;
+        typedef T &reference;
         // Not pure virtual
         virtual int starting_element()
         {
             int myints2[5];
-            MyVector<int, 5> vect(myints2, myints2+5);
-            MyVector<int, 5>::iterator it(vect, 0);
+            MyVector<int> vect(myints2, myints2+5);
+            MyVector<int>::iterator it(vect, 0);
             return 0;
         };
+
+		virtual iterator begin() = 0;
+		virtual iterator end() = 0;
 
         // Not pure virtual
         virtual int last_element()
         {
             int myints2[5];
-            MyVector<int, 5> vect(myints2, myints2+5);
-            MyVector<int, 5>::iterator it(vect, 4);
+            MyVector<int> vect(myints2, myints2+5);
+            MyVector<int>::iterator it(vect, 4);
             return 0;
         }
 
@@ -84,27 +95,27 @@ class MyCollection
         virtual void show_all() = 0;
 };
 
-template<class T, int N>
+template<class T>
 class MyIterator {
     private:
-        MyVector<T, N> &collection;
+        MyVector<T> &collection;
         int offset;
 
     public:
         MyIterator() {}
-        MyIterator(MyVector<T, N> &c, int size) : collection(c), offset(size) {}
+        MyIterator(MyVector<T> &c, int size) : collection(c), offset(size) {}
         //~MyIterator { delete collection; }
         // Assignment
         T &operator*() { return collection[offset]; }
         MyIterator operator++() { ++offset; return *this; }
         MyIterator operator++(int) {
-            MyIterator<T, N> clone(*this);
+            MyIterator<T> clone(*this);
             ++offset;
             return clone;
         }
         MyIterator operator--() { --offset; return *this; }
         MyIterator operator--(int) {
-            MyIterator<T, N> clone(*this);
+            MyIterator<T> clone(*this);
             --offset;
             return clone;
         }
